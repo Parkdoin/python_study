5b31b5b99bd203ac1db82f01c123e7c59b27cb4a
Parkdoin <pdi8095@nate.com>
2021-11-09 13:55:46 +0900

    11월9일오전

--- 변수와문자열.py
+++ 변수와문자열.py
@@ -1,9 +1,9 @@
-# # 여러개의 변수를 동시에 초기화
-# a, b, c = 1, 2, 3
-# print(a, b, c)
-# print(a)
-# print(b)
-# print(c)
+# 여러개의 변수를 동시에 초기화
+a, b, c = 1, 2, 3
+print(a, b, c)
+print(a)
+print(b)
+print(c)
 
 # 예제2
 x = 10
