c6deae255ceb78d19a82463200f5ba33ba9d0361
Parkdoin <pdi8095@nate.com>
2021-11-09 11:25:26 +0900

    시작

--- 파이썬시작.py
+++ 파이썬시작.py
@@ -1,1 +1,13 @@
-print('문자열출력')
+# # 주석은 #, ctrl+/
+# print('문자열출력')
+
+# # 입력
+# x = input('입력할 내용 : ')
+# print(x)
+
+# 예제
+name = input('당신의 이름은 ? ')
+print('안녕하세요', name, '님')
+print('안녕하세요 '+name+' 님')
+print('안녕하세요 %s 님' %name)
+
